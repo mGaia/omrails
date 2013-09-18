15e9c9a0c86962c6b666cefe4e32f01c2e359fa4
melissa <melissa@eternalrealm.ca>
2013-09-17 21:14:46 -0300

    re-route root pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Omrails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+  root :to => 'pins#index'
 
   # See how all your routes lay out with "rake routes"
 
